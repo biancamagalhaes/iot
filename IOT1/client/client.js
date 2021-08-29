6792981bf8ed3a9b03cceb38dbe7b7736a981902
Bianca Magalhães <bianca.magalhaes2610@gmail.com>
2021-08-29 19:21:14 -0300

    only server

--- IOT1/client/client.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-axios.get('http://localhost:3000')
-  .then(function (response) {
-    console.log(response.data);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers);
-    console.log(response.config);
-  });
