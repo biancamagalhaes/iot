1caa05bf798e7b4de3e9b6281ef6d3bad0bdcc5a
Bianca Magalhães <bianca.magalhaes2610@gmail.com>
2021-08-29 20:04:48 -0300

    remove path and add socket.io

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { Server } = require("socket.io");
+const io = new Server(server);
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('IOT1')
 })
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at https://ies-iot-1.herokuapp.com:8080`)
