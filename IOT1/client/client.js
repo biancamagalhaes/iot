7707921b344d46db24756a1d7f76d920a0010c47
Bianca Magalhães <bianca.magalhaes2610@gmail.com>
2021-08-29 19:18:17 -0300

    initial commit

---
+++ IOT1/client/client.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+axios.get('http://localhost:3000')
+  .then(function (response) {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  });
