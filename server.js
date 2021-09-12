86280a3a78178692e8acdac195f340d61df545c0
Bianca Magalhães <bianca.magalhaes2610@gmail.com>
2021-09-12 19:28:06 -0300

    part 2

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var net = require('net');
+var connectedDevices = [];
+var nameServer = 'servidor_bianca';
+var state = 'ATIVADO';
 
 var server = net.createServer();
 server.on('connection', (connect) => {
+
   connect.on('data', (message) => {
-    console.log(message);
-    connect.write('Você conseguiu se conectar, sua mensagem: ', message);
+    
+    var name = message.toString().split(' ')[0];
+
+    if(!connectedDevices.includes(name)){
+      console.log(message.toString());
+      var string = (nameServer + ' ' + state + ' 40').toString();
+      connect.write(string);
+      connectedDevices.push(name);
+    }
+
   });
 
   connect.on('close', () => {
