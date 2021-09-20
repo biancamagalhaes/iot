08a7baf7d04d0eb3d7886a0b7deac77632e9ad47
Bianca Magalhães <bianca.magalhaes2610@gmail.com>
2021-09-19 21:11:12 -0300

    new code

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
 var net = require('net');
+var prompt = require('prompt');
 var connectedDevices = [];
 var nameServer = 'servidor_bianca';
 var state = 'ATIVADO';
 
+prompt.start();
+
+console.log('O servidor está ATIVO [true/false]?');
+prompt.get('isActive', function (err, result) {
+    if (err) { return onErr(err); }
+    if(result.isActive === 'true'){
+      state = 'ATIVADO'
+    }else{
+      state = 'DESATIVADO'
+    }
+  });
+
 var server = net.createServer();
+
 server.on('connection', (connect) => {
 
   connect.on('data', (message) => {
+
+    var remoteAddress = connect.remoteAddress;
+    var remotePort = connect.remotePort;
     
     var name = message.toString().split(' ')[0];
 
-    if(!connectedDevices.includes(name)){
+    if(!connectedDevices.some(e => e.name === name)){
       console.log(message.toString());
       var string = (nameServer + ' ' + state + ' 40').toString();
       connect.write(string);
-      connectedDevices.push(name);
     }
+    
+    connectedDevices.push({name, address: remoteAddress + ': ' + remotePort});
+
+    setInterval(() => {
+      var temperature = Math.random();
+      connect.write(temperature.toString() + '° C');
+    }, 40000);
 
   });
 
